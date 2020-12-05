1aa00d235a48c0da4eb05b4725fed5b163552b78
itsUrcute <riniefrinoo@gmail.com>
2020-12-05 23:13:27 +0530

    day 5 completed

---
+++ 2020/day 5/day5_2.py
@@ -0,0 +1,30 @@
+with open("input.txt", "r") as file:
+    seatlist = file.read().split("\n")
+
+def sol(list):
+    fl=[]
+    k=[]
+    o=[0,1,2,3,4,5,6,7]
+    for i in range(128):
+      k.append(i)
+    for i in seatlist:
+        seatrow=k
+        seatcol=o
+        a,b = i[0:7],i[7:10]
+        for q in a:
+            if q=="F":
+                seatrow=seatrow[0:int(len(seatrow)/2)]
+            elif q=="B":
+                seatrow=seatrow[int(len(seatrow)/2):int(len(seatrow))]
+        for w in b:
+            if w=="L":
+                seatcol=seatcol[0:int(len(seatcol)/2)]
+            elif w=="R":
+                seatcol=seatcol[int(len(seatcol)/2):int(len(seatcol))]
+        fl.append(seatrow[0]*8+seatcol[0])
+        fl.sort()
+    for o in fl:
+        if o+2 in fl and o+1 not in fl:
+            return(o+1)
+
+print(f"day 5, solution 2 = {sol(seatlist)}")
